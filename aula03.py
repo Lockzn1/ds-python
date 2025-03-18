ffb2788a5d3e1c3b82998cc43f2a8124e6ac487e
Murilo Cotrim <cotrim.murilo@gmail.com>
2025-03-18 20:39:43 -0300

    aula04

---
+++ aula03.py
@@ -0,0 +1,51 @@
+#valor = float(input("informe valor da prestção: "))
+#taxa = float(input("informe a taxa mensal: "))
+#tempo = float(input("Informe quantidade de mes atrasado: "))
+#prest = valor + (valor*(taxa/100)*tempo)
+#print("O valor calculado é: ", prest)
+ 
+ #variaveis
+ 
+#var1 = 123
+#var2 = 'World'
+#print("Hello to the",var2,var1)
+#print("Hello to the %s %d " %(var2,var1))
+#print("Hello to the {} {}".format(var2,var1))
+#print("Hello to the " +var2 +" " +str(var1))
+#print(f"Hello to the {var2} {var1}")
+ 
+ #atividades 1-4  
+ 
+ #1
+#P1 = float(input("Informe a nota 1: "))      
+#P2 = float(input("Informe a nota 2: "))      
+#P3 = float(input("Informe a nota 3: "))    
+#media = (P1 + P2 + P3)/3
+#print("A sua média é: ",media)
+ 
+ #2
+#A = 3
+#B = 4
+#hip = (3*3 + 4*4) **0.5
+#print("A raiz é:",hip )
+ 
+ #3
+#salario = float(input("Informe seu salário: "))
+#aumento = float(input("Informe o aumento percentual: "))
+#novosalario = salario + (salario * aumento/100)
+#print("O novo salário é: ",novosalario)
+ 
+ #4
+#pi = 3.1415
+#altura = float(input("Informe a altura: "))
+#raio = float(input("Informe o raio: "))
+#volume = pi * raio **2 * altura
+#print("O volume é: ",volume)
+ 
+#condicional
+ 
+#media = float(input("Informe a média: "))
+#if media >= 5:
+#    print('Aprovado!')
+#else :
+#    print('Reprovado!')
---
+++ aula04/atividade.py
@@ -0,0 +1,52 @@
+#ATIVIDADE
+
+#1
+#timeA= float(input("Informe o(s) gol(s) do time A: "))
+#timeB= float(input("Informe o(s) gol(s) do time B: "))
+#if timeA > timeB :
+#    print('Vitoria time A')
+#elif timeA == timeB :
+#    print('Empate')
+#else:
+#    print('Vitoria time B')
+
+#2
+#compra= float(input("Informe o valor da compra: "))
+#prest= float(input("Informe o número de parcelas: "))
+#
+#parc= compra/prest
+#print(parc)
+
+#3
+#idade = float(input("Informe a idade do nadador: "))
+#
+#if 5 <= idade <= 7:
+#    print('Categoria: Infantil A')
+#elif 8 <= idade <= 11:
+#    print('Categoria: Infantil B')
+#elif 12 <= idade <= 13:
+#    print('Categoria: Juvenil A')
+#elif 14 <= idade <= 17:
+#    print('Categoria: Juvenil B')
+#elif idade >=18:
+#    print('Categoria: Adulto')
+#else:
+#    print('Sem categoria')
+
+
+#4
+#num1= float(input("Informe o primeiro número: "))
+#num2= float(input("Informe o segundo número: "))
+#
+#oper=(input("Informe a operação: "))
+#
+#if oper == '+':
+#    print(num1+num2)
+#elif oper == '-':
+#    print(num1-num2)
+#elif oper == '*':
+#    print(num1*num2)
+#elif oper == '/':
+#    print(num1/num2)
+#else:
+#    print("Erro")
